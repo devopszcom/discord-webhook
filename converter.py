e7305fc82f1fbe29e8236cfbaf26217794433f9d
Cuong Tran <cuongtransc@gmail.com>
2019-01-20 09:25:50 +0700

    Update converter: try catch when extract title from data

--- converter.py
+++ converter.py
@@ -1,3 +1,8 @@
+import logging
+
+logger = logging.getLogger('converter')
+
+
 def convert_sentry(data):
     attachment = dict()
     attachment['color'] = "#f43f20"
@@ -13,9 +18,13 @@ def convert_sentry(data):
         'short': False,
     })
 
-    attachment['fallback'] = '[{}] {}'.format({data["project"]}, data['event']['metadata']['title'])
-    attachment['title_link'] = data['url']
-    attachment['title'] = data['event']['metadata']['title']
+    try:
+        attachment['fallback'] = '[{}] {}'.format({data["project"]}, data['event']['metadata'].get('title', ''))
+
+        attachment['title_link'] = data['url']
+        attachment['title'] = data['event']['metadata'].get('title', '')
+    except Exception:
+        logger.warning('Can not extract title from data: {}'.format(data))
 
     discord_data = dict()
     # discord_data['username'] = 'Sentry'
